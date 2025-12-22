732db1874976caaaafadb0dd1cc584cb931de8c0
Sha7ata-Mohamed <sha7ata1379@gmail.com>
2025-12-22 17:17:46 +0300

    d
    d

--- JS-T/script.js
+++ JS-T/script.js
@@ -1,52 +1,49 @@
-console.log("IT works!");
+const item = document.getElementById("list");
+item.innerHTML = "";
 
-const name = "Abdo";
-let age = 22;
-
-const names = "Mohammed";
-let ages = 54;
-
-
-console.log(`My name is ${name} and my age is ${age}`);
-console.log("My name is " + name + " and my age is " + age);
-
-console.log('My dad name is', names, 'and his age is', ages);
-console.log('My dad name is' + names + ' and his age is ' + ages);
-console.log(`My dad name is ${names} and his age is ${ages}`);
-
-const x = 12;
-if (x % 2 === 0) console.log("even"); else console.log("odd");
-
-
-for (let i = 0; i < 3; i++) console.log(i);
-
-
-
-let total = 0;
-for ( let x = 1; x <= 100; x++) {
-    total += x;
-}
-console.log(total);
-
-const day = "Mon";
-switch (day){
-    case "Sun":
-        console.log("Sunday");
-        break;
-    case "Sat":
-        console.log("Saturday");
-        break;
-    case "Mon":
-        console.log("Monday");
-        break;
+for (let i = 1; i < 5; i++) {
+    item.innerHTML += "<li>Item " + i + "</li>";
 }
 
-function add(a, b = 0) { return a + b; }
-const mul = (a, b) => a * b;
+document.querySelectorAll(".ids").forEach(h => {
+    h.style.backgroundColor = "red";
+    h.style.color = "white";
+    h.style.borderRadius = "20px";
+});
+document.querySelectorAll(".msg").forEach(p => {
+    p.style.backgroundColor = "green";
+    p.style.color = "white";
+});
 
-function makeCounter() {
-  let c = 0;                 // private via closure
-  return () => ++c;
+// console.log(changeStyleHeader);
+
+function $(selector) {
+    return document.querySelector(selector);
 }
-const next = makeCounter();
-console.log(next(), next()); // 1 2
+function $$(selector) {
+    return document.querySelectorAll(selector);
+}
+
+function getID(selector) {
+    return document.getElementById(selector);
+}
+
+function list(selector) {
+    return document.classList(selector);
+}
+
+
+let l = list("class-list");
+console.log(l);
+
+
+let node = $("#msg");
+console.log(node);
+
+let n = $$("#msg");
+console.log(n);
+
+let i = getID("id");
+console.log(i);
+
+
